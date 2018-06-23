ddba6fce3b767d5658231ee9288e4dd2e7a45f23
Martin <martin.berger.developer@gmail.com>
2018-06-23 19:32:40 +0200

    Installed Carthage dependency manager. Added PureLayout to project.

---
+++ ThoughtForTheDay/ThoughtForTheDay/TriggersViewController/TriggersViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TriggersViewController.swift
+//  ThoughtForTheDay
+//
+//  Created by Martin on 6/23/18.
+//  Copyright © 2018 heavydebugging. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import PureLayout
+
+class TriggersViewController: UIViewController {
+    var tableView: UITableView!
+}
