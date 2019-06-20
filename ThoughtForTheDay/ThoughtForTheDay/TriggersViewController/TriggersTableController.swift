30206771dc81125ba2c89148a46db21ae4d78b51
Martin Berger <martin.berger.developer@gmail.com>
2019-06-20 18:04:01 +0200

    Updated to Swift 5 syntax.

--- ThoughtForTheDay/ThoughtForTheDay/TriggersViewController/TriggersTableController.swift
+++ ThoughtForTheDay/ThoughtForTheDay/TriggersViewController/TriggersTableController.swift
@@ -66,11 +66,11 @@ extension TriggersTableController: UITableViewDelegate {
         return true
     }
     
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             self.dataProvider.setToCancel(indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
